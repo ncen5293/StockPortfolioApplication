945aa0d05b9c94ecd69a4e919179be0100b4cd6f
Nicky Cen <nickycen@hotmail.com>
2019-07-11 14:20:53 -0400

    initial client and server side

---
+++ routes/stocks.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+let databaseConnection = "Waiting for Database response...";
+
+router.get("/", function(req, res, next) {
+    res.send(databaseConnection);
+});
+
+mongoose.connect("mongodb://mongodb:27017/test");
+
+mongoose.connection.on("error", error => {
+    console.log("Database connection error:", error);
+    databaseConnection = "Error connecting to Database";
+});
+
+// If connected to MongoDB send a success message
+mongoose.connection.once("open", () => {
+    console.log("Connected to Database!");
+    databaseConnection = "Connected to Database";
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const stockRouter = require("./routes/stocks");
+
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/stockapi", stockRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+module.exports = app;
